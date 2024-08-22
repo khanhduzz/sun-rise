e33def390977c37f2256a59e5eeb19875e2b70e7
Khanh Du <luongkhanhdu95@gmail.com>
2024-08-22 22:40:45 +0700

    Add health check and application.yml

---
+++ src/main/java/com/fjb/sunrise/controllers/HealthController.java
@@ -0,0 +1,17 @@
+package com.fjb.sunrise.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/health")
+public class HealthController {
+    @GetMapping
+    public ModelAndView health() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("health");
+        return modelAndView;
+    }
+}
