d29d9d24c85aceabb7a4973d92d141d9566825bf
Khanh Du <>
2024-08-22 22:02:22 +0700

    init project

---
+++ src/main/java/com/fjb/sunrise/SunRiseApplication.java
@@ -0,0 +1,13 @@
+package com.fjb.sunrise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunRiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunRiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fjb/sunrise/SunRiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fjb.sunrise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunRiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
