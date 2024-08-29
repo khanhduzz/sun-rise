a0ac93f4b00d9db192c2fa682fd1df9d4d9a6c23
Hoang An <nightmarelod9@gmail.com>
2024-08-29 14:38:17 +0700

    delete unuse import

--- src/main/java/com/fjb/sunrise/models/Transaction.java
+++ src/main/java/com/fjb/sunrise/models/Transaction.java
@@ -1,12 +1,10 @@
 package com.fjb.sunrise.models;
 
 import com.fjb.sunrise.enums.ETrans;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,7 +12,6 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
