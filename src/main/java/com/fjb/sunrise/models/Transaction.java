d8b2447e3b90e34014a2fbcce91829923b39584c
VoHoangAn <63175067+VoHoangAn@users.noreply.github.com>
2024-08-30 07:13:52 +0700

    add transaction entity (#21)
    * add transaction entity
    
    * delete unuse import

--- src/main/java/com/fjb/sunrise/models/Transaction.java
+++ src/main/java/com/fjb/sunrise/models/Transaction.java
@@ -1,28 +1,51 @@
 package com.fjb.sunrise.models;
 
+import com.fjb.sunrise.enums.ETrans;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
-@Builder
 @Table(name = "transactions")
 @AllArgsConstructor
 @NoArgsConstructor
-public class Transaction extends AuditEntity<Long> {
+@ToString
+public class Transaction extends AuditEntity<String> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true)
     private Long id;
 
-    private String transactionType;
+    @Column(columnDefinition = "DECIMAL(11,2)")
+    private Double amount;
+
+    @Enumerated(value = EnumType.STRING)
+    private ETrans transactionType;
+
+    @Column(columnDefinition = "TEXT")
+    private String note;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Category category;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 }
