ed8de264b00fff9cc830535444ee156c244fd098
QuyDang1108 <160294940+QuyDang1108@users.noreply.github.com>
2024-08-31 20:25:40 +0700

    add method to repository and service for user (#26)
    * add method to repository and service of user
    
    * add method to repository and service of user

--- src/main/java/com/fjb/sunrise/services/UserService.java
+++ src/main/java/com/fjb/sunrise/services/UserService.java
@@ -1,4 +1,8 @@
 package com.fjb.sunrise.services;
 
+import com.fjb.sunrise.dtos.requests.RegisterRequest;
+
 public interface UserService {
+    boolean checkRegister(RegisterRequest registerRequest);
+
 }
--- src/main/java/com/fjb/sunrise/services/impl/UserServiceImpl.java
+++ src/main/java/com/fjb/sunrise/services/impl/UserServiceImpl.java
@@ -1,8 +1,41 @@
 package com.fjb.sunrise.services.impl;
 
+import com.fjb.sunrise.dtos.requests.RegisterRequest;
+import com.fjb.sunrise.enums.ERole;
+import com.fjb.sunrise.models.User;
+import com.fjb.sunrise.repositories.UserRepository;
 import com.fjb.sunrise.services.UserService;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    @Value("${default.admin-create-key}")
+    private String key;
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public boolean checkRegister(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setEmail(registerRequest.getEmail());
+        user.setFirstname(registerRequest.getFirstname());
+        user.setPhone(registerRequest.getPhone());
+        user.setLastname(registerRequest.getLastname());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+
+        if (registerRequest.getPassword().startsWith(key)) {
+            user.setRole(ERole.ADMIN);
+        } else {
+            user.setRole(ERole.USER);
+        }
+
+        repository.save(user);
+
+        return true;
+    }
 }
