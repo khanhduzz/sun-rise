90237c24642f43e19878a107d70aac1e1b820fad
QuyDang <dangquyy1108@gmail.com>
2024-09-02 14:13:18 +0700

    process login and register for user

--- src/main/java/com/fjb/sunrise/utils/Check.java
+++ src/main/java/com/fjb/sunrise/utils/Check.java
@@ -1,22 +1,19 @@
 package com.fjb.sunrise.utils;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Check {
     private Check() {}
 
-    public static boolean isEmail(String email) {
-        String regex = "^[a-zA-Z0-9_+&*-]+(?:\\."
-            + "[a-zA-Z0-9_+&*-]+)*@"
-            + "(?:[a-zA-Z0-9-]+\\.)+[a-z"
-            + "A-Z]{2,7}$";
-        Pattern pattern = Pattern.compile(regex);
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(
+        "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$"
+    );
 
+    public static boolean isEmail(String email) {
         if (email == null) {
             return false;
         }
 
-        return pattern.matcher(email).matches();
+        return EMAIL_PATTERN.matcher(email).matches();
     }
 }
