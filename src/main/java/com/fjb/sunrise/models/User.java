26e992b606e9b1fff50a86b1084b5f3f0637162b
Khanh Du <luongkhanhdu95@gmail.com>
2024-09-02 15:30:48 +0700

    remove redundant import in user

--- src/main/java/com/fjb/sunrise/models/User.java
+++ src/main/java/com/fjb/sunrise/models/User.java
@@ -10,7 +10,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 import lombok.AllArgsConstructor;
