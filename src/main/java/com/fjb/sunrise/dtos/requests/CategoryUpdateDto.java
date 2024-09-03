d51f2d184dc5a994a54e759d025b42e442b9f9c7
Long Tran <tranngocthienlong12@gmail.com>
2024-09-03 14:25:49 +0700

    create category

---
+++ src/main/java/com/fjb/sunrise/dtos/requests/CategoryUpdateDto.java
@@ -0,0 +1,17 @@
+package com.fjb.sunrise.dtos.requests;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.io.Serializable;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CategoryUpdateDto implements Serializable {
+    @NotBlank(message = "Vui lòng nhâp tên danh mục")
+    @Size(max = 50, message = "Không vượt quá 50 ký tự")
+    private String name;
+}
