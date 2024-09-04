c3fee19d09fa4733c48894e1699f220942841a14
Longlay12 <112888879+Longlay12@users.noreply.github.com>
2024-09-04 19:33:55 +0700

    [Category] Update/category (#42)
    * update category
    
    Co-authored-by: NAME khanhduzz
    Co-authored-by: ANOTHER-NAME luongkhanhdu95@gmail.com

---
+++ src/main/java/com/fjb/sunrise/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.fjb.sunrise.repositories;
+
+import com.fjb.sunrise.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
