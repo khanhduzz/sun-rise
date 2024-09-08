61304135be69e2c4c57cd05b3ae157f43e895f20
BinhDT1707 <thanhbinh5595@gmail.com>
2024-09-08 21:35:35 +0700

    push admin application

--- src/main/java/com/fjb/sunrise/mappers/UserMapper.java
+++ src/main/java/com/fjb/sunrise/mappers/UserMapper.java
@@ -1,10 +1,15 @@
 package com.fjb.sunrise.mappers;
 
 import com.fjb.sunrise.dtos.requests.RegisterRequest;
+import com.fjb.sunrise.dtos.user.EditProfileByAdminDTO;
 import com.fjb.sunrise.models.User;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     User toEntity(RegisterRequest request);
+
+    User toEntityByAdmin(EditProfileByAdminDTO byAdminDTO);
+
+    EditProfileByAdminDTO toUserByAdmin(User user);
 }
