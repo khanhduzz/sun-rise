14448706ae13a39b168a590ffa942a5b31250385
Khanh Du <110228244+khanhduzz@users.noreply.github.com>
2024-09-12 13:37:53 +0700

    fix bug and refactor code (#75)
    * refactor bug and code
    
    * ignore gitleaks
    
    * fix
    
    * fix sonar

--- src/main/java/com/fjb/sunrise/dtos/responses/TransactionFullPageResponse.java
+++ src/main/java/com/fjb/sunrise/dtos/responses/TransactionFullPageResponse.java
@@ -1,14 +1,12 @@
 package com.fjb.sunrise.dtos.responses;
 
-import jakarta.persistence.Transient;
 import java.io.Serializable;
 import java.util.List;
 import lombok.Data;
 
 @Data
 public class TransactionFullPageResponse implements Serializable {
-    @Transient
-    private List<TransactionPageResponse> data;
+    private transient List<TransactionPageResponse> data;
     private Long recordsTotal;
     private Long recordsFiltered;
     private Integer draw;
