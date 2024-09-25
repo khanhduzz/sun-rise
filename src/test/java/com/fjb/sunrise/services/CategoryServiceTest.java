efe348ed57ae18e329789da88afb576b57baf998
longlay12 <tranngocthienlong12@gmail.com>
2024-09-25 11:21:49 +0700

    add unit test

--- src/test/java/com/fjb/sunrise/services/CategoryServiceTest.java
+++ src/test/java/com/fjb/sunrise/services/CategoryServiceTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
 import com.fjb.sunrise.dtos.base.DataTableInputDTO;
 import com.fjb.sunrise.dtos.requests.CategoryCreateDto;
