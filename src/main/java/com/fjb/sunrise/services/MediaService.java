405d87f7e89b45bcf22708c340b160179b8ac98d
Khanh Du <110228244+khanhduzz@users.noreply.github.com>
2024-09-27 16:05:44 +0700

    setup show avatar (#152)

--- src/main/java/com/fjb/sunrise/services/MediaService.java
+++ src/main/java/com/fjb/sunrise/services/MediaService.java
@@ -1,12 +1,16 @@
 package com.fjb.sunrise.services;
 
 import com.fjb.sunrise.exceptions.BadRequestException;
+import com.fjb.sunrise.exceptions.NotFoundException;
 import com.fjb.sunrise.models.Media;
+import com.fjb.sunrise.models.User;
 import com.fjb.sunrise.repositories.MediaRepository;
+import com.fjb.sunrise.repositories.UserRepository;
 import java.io.IOException;
 import java.util.UUID;
 import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
@@ -18,6 +22,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class MediaService {
 
     private final MediaRepository mediaRepository;
+    private final UserRepository userRepository;
 
     @Transactional
     public Media store(MultipartFile file) {
@@ -49,5 +54,18 @@ public class MediaService {
     @Transactional
     public Stream<Media> getAllMedias() {
         return mediaRepository.findAll().stream();
+    }
+
+    @Transactional
+    public Media getMediaOfUser() {
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByEmailOrPhone(name);
+        if (user == null) {
+            throw new NotFoundException("User not found");
+        }
+        if (user.getFileCode() == null) {
+            return new Media();
+        }
+        return getMedia(user.getFileCode());
     }
 }
