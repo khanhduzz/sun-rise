e44a229b69727100857a3b20090ea3e42dd025a5
Khanh Du <luongkhanhdu95@gmail.com>
2025-06-30 15:19:47 +0700

    Revert to using user from spring security to make web working

--- src/main/java/com/fjb/sunrise/config/security/SecurityConfig.java
+++ src/main/java/com/fjb/sunrise/config/security/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
             )
             .formLogin(form -> form
                 .loginPage("/auth/login")
-                .defaultSuccessUrl("/v2/home", true)
+                .defaultSuccessUrl("/transaction/create", true)
                 .permitAll()
             )
             .logout(logout -> logout
